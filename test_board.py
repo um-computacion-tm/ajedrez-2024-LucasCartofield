f0141dd5fe75b4edf6b08e70b37af16d1cf3c85f
LucasCartofield <l.cartofield@alumno.um.edu.ar>
2024-08-18 22:35:58 -0300

    adding potential test for board.py

---
+++ test_board.py
@@ -0,0 +1,29 @@
+import unittest
+from rook import Rook
+from board import Board
+
+class TestBoard(unittest.TestCase):
+    
+    def test_initial_positions(self):
+        board = Board()
+
+        # Testing the black rooks' initial positions
+        self.assertIsInstance(board.get_piece(0, 0), Rook)      #assertIsInstance checks if the object is an instance of the 'rook' class
+        self.assertEqual(board.get_piece(0, 0).color, "BLACK")  #asserEqual checks that the rook in this specific position is the correct color
+        self.assertIsInstance(board.get_piece(0, 7), Rook)
+        self.assertEqual(board.get_piece(0, 7).color, "BLACK")
+        
+        # Testing the white rooks' initial positions
+        self.assertIsInstance(board.get_piece(7, 0), Rook)
+        self.assertEqual(board.get_piece(7, 0).color, "WHITE")
+        self.assertIsInstance(board.get_piece(7, 7), Rook)
+        self.assertEqual(board.get_piece(7, 7).color, "WHITE")
+
+        # Testing that the remaining board positions are empty (None)
+        for row in range(8):
+            for col in range(8):
+                if (row, col) not in [(0, 0), (0, 7), (7, 0), (7, 7)]:
+                    self.assertIsNone(board.get_piece(row, col))
+
+if __name__ == '__main__':
+    unittest.main()
