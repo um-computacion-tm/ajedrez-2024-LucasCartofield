a3acf8dcf7e8976e7bf6e3a3c80afb07dd21b3db
LucasCartofield <l.cartofield@alumno.um.edu.ar>
2024-09-01 23:01:04 -0300

    added name to README and rewrote test_board.py

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ajedrez-2024-LucasCartofield
-ajedrez-2024-LucasCartofield created by GitHub Classroom
+
+AUTOR: LUCAS CARTOFIELD
 
 # CircleCI
 [![CircleCI](https://dl.circleci.com/status-badge/img/gh/um-computacion-tm/ajedrez-2024-LucasCartofield/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/um-computacion-tm/ajedrez-2024-LucasCartofield/tree/main)
--- test_board.py
+++ test_board.py
@@ -8,75 +8,30 @@ from pawn import Pawn
 from board import Board
 
 class TestBoard(unittest.TestCase):
-
+    
     def test_initial_positions(self):
         board = Board()
 
-        # Testing the black rooks' initial positions
-        self.assertIsInstance(board.get_piece(0, 0), Rook)
-        self.assertEqual(board.get_piece(0, 0).get_color(), "BLACK")
-        self.assertIsInstance(board.get_piece(0, 7), Rook)
-        self.assertEqual(board.get_piece(0, 7).get_color(), "BLACK")
-        
-        # Testing the white rooks' initial positions
-        self.assertIsInstance(board.get_piece(7, 0), Rook)
-        self.assertEqual(board.get_piece(7, 0).get_color(), "WHITE")
-        self.assertIsInstance(board.get_piece(7, 7), Rook)
-        self.assertEqual(board.get_piece(7, 7).get_color(), "WHITE")
+        # Expected positions for each type of piece
+        starting_positions = {
+            Rook: [("BLACK", [(0, 0), (0, 7)]), ("WHITE", [(7, 0), (7, 7)])],
+            Knight: [("BLACK", [(0, 1), (0, 6)]), ("WHITE", [(7, 1), (7, 6)])],
+            Bishop: [("BLACK", [(0, 2), (0, 5)]), ("WHITE", [(7, 2), (7, 5)])],
+            King: [("BLACK", [(0, 3)]), ("WHITE", [(7, 3)])],
+            Queen: [("BLACK", [(0, 4)]), ("WHITE", [(7, 4)])],
+            Pawn: [("BLACK", [(1, i) for i in range(8)]), ("WHITE", [(6, i) for i in range(8)])],
+        }
 
-        # Testing the black knights' initial positions
-        self.assertIsInstance(board.get_piece(0, 1), Knight)
-        self.assertEqual(board.get_piece(0, 1).get_color(), "BLACK")
-        self.assertIsInstance(board.get_piece(0, 6), Knight)
-        self.assertEqual(board.get_piece(0, 6).get_color(), "BLACK")
+        # Test each piece's initial positions on the board
+        for piece, color_positions in starting_positions.items():
+            for color, positions in color_positions:
+                for row, col in positions:
+                    piece_instance = board.get_piece(row, col)
+                    self.assertIsInstance(piece_instance, piece)
+                    self.assertEqual(piece_instance.get_color(), color)
 
-        # Testing the white knights' initial positions
-        self.assertIsInstance(board.get_piece(7, 1), Knight)
-        self.assertEqual(board.get_piece(7, 1).get_color(), "WHITE")
-        self.assertIsInstance(board.get_piece(7, 6), Knight)
-        self.assertEqual(board.get_piece(7, 6).get_color(), "WHITE")
-
-        # Testing the black bishops' initial positions
-        self.assertIsInstance(board.get_piece(0, 2), Bishop)
-        self.assertEqual(board.get_piece(0, 2).get_color(), "BLACK")
-        self.assertIsInstance(board.get_piece(0, 5), Bishop)
-        self.assertEqual(board.get_piece(0, 5).get_color(), "BLACK")
-
-        # Testing the white bishops' initial positions
-        self.assertIsInstance(board.get_piece(7, 2), Bishop)
-        self.assertEqual(board.get_piece(7, 2).get_color(), "WHITE")
-        self.assertIsInstance(board.get_piece(7, 5), Bishop)
-        self.assertEqual(board.get_piece(7, 5).get_color(), "WHITE")
-
-        # Testing the black king's initial position
-        self.assertIsInstance(board.get_piece(0, 3), King)
-        self.assertEqual(board.get_piece(0, 3).get_color(), "BLACK")
-
-        # Testing the white king's initial position
-        self.assertIsInstance(board.get_piece(7, 3), King)
-        self.assertEqual(board.get_piece(7, 3).get_color(), "WHITE")
-
-        # Testing the black queen's initial position
-        self.assertIsInstance(board.get_piece(0, 4), Queen)
-        self.assertEqual(board.get_piece(0, 4).get_color(), "BLACK")
-
-        # Testing the white queen's initial position
-        self.assertIsInstance(board.get_piece(7, 4), Queen)
-        self.assertEqual(board.get_piece(7, 4).get_color(), "WHITE")
-
-        # Testing the black pawns' initial positions
-        for col in range(8):
-            self.assertIsInstance(board.get_piece(1, col), Pawn)
-            self.assertEqual(board.get_piece(1, col).get_color(), "BLACK")
-
-        # Testing the white pawns' initial positions
-        for col in range(8):
-            self.assertIsInstance(board.get_piece(6, col), Pawn)
-            self.assertEqual(board.get_piece(6, col).get_color(), "WHITE")
-
-        # Testing that the remaining board positions are empty (None)
-        occupied_positions = [(0, i) for i in range(8)] + [(1, i) for i in range(8)] + \
-                             [(7, i) for i in range(8)] + [(6, i) for i in range(8)]
+        # Test that all other positions are empty
+        occupied_positions = {pos for positions in starting_positions.values() for _, pos_list in positions for pos in pos_list}
         for row in range(8):
             for col in range(8):
                 if (row, col) not in occupied_positions:
