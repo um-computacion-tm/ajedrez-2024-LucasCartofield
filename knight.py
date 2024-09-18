5ab9423af250a93aec7ac8b6129cc47bd54ada90
LucasCartofield <l.cartofield@alumno.um.edu.ar>
2024-09-17 23:34:46 -0300

    added possible knight moves to file

--- knight.py
+++ knight.py
@@ -5,5 +5,21 @@ class Knight(Piece):
     white_str = "♞"
     black_str = "♘"
 
-    def get_possible_positions(self, from_row, from_col):
-        return ()
+    
+def knight_moves(position):
+    moves = [
+        (2, 1), (2, -1), (-2, 1), (-2, -1),
+        (1, 2), (1, -2), (-1, 2), (-1, -2)
+    ]
+    
+    row, col = position
+    
+    valid_moves = []
+    for move in moves:
+        new_row = row + move[0]
+        new_col = col + move[1]
+        
+        if 0 <= new_row < 8 and 0 <= new_col < 8:
+            valid_moves.append((new_row, new_col))
+    
+    return valid_moves
