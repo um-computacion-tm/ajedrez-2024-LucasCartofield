930fd1ad23dcf3a4a4a1f4168ce938d728462d71
LucasCartofield <l.cartofield@alumno.um.edu.ar>
2024-09-22 21:39:14 -0300

    added pawn promotion method to board class

--- board.py
+++ board.py
@@ -7,7 +7,6 @@ from pawn import Pawn
 from exceptions import OutOfBoard
 
 
-
 class Board:
     def __init__(self, for_test = False):
         self.__positions__ = []
@@ -16,31 +15,20 @@ class Board:
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-            
         if not for_test:
-            starting_place = {
-                Rook: [("BLACK", [(0, 0), (0, 7)]), ("WHITE", [(7, 0), (7, 7)])],
-                Knight: [("BLACK", [(0, 1), (0, 6)]), ("WHITE", [(7, 1), (7, 6)])],
-                Bishop: [("BLACK", [(0, 2), (0, 5)]), ("WHITE", [(7, 2), (7, 5)])],
-                King: [("BLACK", [(0, 3)]), ("WHITE", [(7, 3)])],
-                Queen: [("BLACK", [(0, 4)]), ("WHITE", [(7, 4)])],
-                Pawn: [("BLACK", [(1, i) for i in range(8)]), ("WHITE", [(6, i) for i in range(8)])],
-            }
-
-            for piece, color_positions in starting_place.items():
-                for color, positions in color_positions:
-                    for position in positions:
-                        row, col = position
-                        self.__positions__[row][col] = piece(color, self)
+            self.__positions__[0][0] = Rook("BLACK", self) # Black
+            self.__positions__[0][7] = Rook("BLACK", self) # Black
+            self.__positions__[7][7] = Rook("WHITE", self) # White
+            self.__positions__[7][0] = Rook("WHITE", self) # White
 
     def __str__(self):
         board_str = ""
         for row in self.__positions__:
-            for piece in row:
-                if piece is None:
-                    board_str += ". "  # for empty spaces
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
                 else:
-                    board_str += piece.__str__() + " "
+                    board_str += " "
             board_str += "\n"
         return board_str
 
@@ -53,8 +41,17 @@ class Board:
 
     def set_piece(self, row, col, piece):
         self.__positions__[row][col] = piece
-    
+
     def move(self, from_row, from_col, to_row, to_col):
         origin = self.get_piece(from_row, from_col)
         self.set_piece(to_row, to_col, origin)
         self.set_piece(from_row, from_col, None)
+
+        # Check if the moved piece is a pawn
+        if isinstance(origin, Pawn):
+            # Check if the pawn reached the last row (row 0 for white, row 7 for black)
+            if (origin.get_color() == "WHITE" and to_row == 0) or (origin.get_color() == "BLACK" and to_row == 7):
+                # Promote the pawn to a queen
+                promoted_piece = Queen(origin.get_color(), self)
+                self.set_piece(to_row, to_col, promoted_piece)
+                print(f"Pawn at ({to_row}, {to_col}) promoted to Queen.")
